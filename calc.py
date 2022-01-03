2b70940576bc801a46cd9e3238d32f4d054b7b90
Abhilash U Prakash <66258607+Abhilashup@users.noreply.github.com>
2022-01-03 16:32:45 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -8,11 +8,6 @@ def subtract(a, b):
 def multiply(a, b):
     return a*b
 
-def divide(a, b):
-    if b==0:
-        raise ValueError("Cannot divide by zero!")
-    return a/b
-
 
 res = add(2, 3)
 print(res)
