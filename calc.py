b80781310b7310629bceb746893d16e8d913375e
Abhilash U Prakash <66258607+Abhilashup@users.noreply.github.com>
2022-01-07 10:00:25 +0530

    Adding comments

--- calc.py
+++ calc.py
@@ -1,14 +1,17 @@
+#Function to add numbers
 def add(a, b):
     return a+b
         
-
+#Function to subtract numbers
 def subtract(a, b):
     return a-b
 
+#Function to multiply numbers
 def multiply(a, b):
     return a*b
 
 
+#Checking if the function works
 res = add(1, 4)
 print(res)
 
