db4d2d4f99b72c9ae670442916baa7487a7ab123
Abhilash U Prakash <66258607+Abhilashup@users.noreply.github.com>
2022-01-07 18:46:25 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,19 +1,33 @@
-#Function to add numbers
 def add(a, b):
-    return a+b
+    return int(a)+int(b)
         
-#Function to subtract numbers
+
 def subtract(a, b):
-    return a-b
+    return int(a)-int(b)
 
-#Function to multiply numbers
 def multiply(a, b):
-    return a*b
+    return int(a)*int(b)
 
 
-#Checking if the function works properly
-res = add(1, 4)
-print(res)
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+operation = input(str("Enter the operation : "))
+
+if operation == "+":
+    print(add(num1,num2))
+
+elif operation == "-":
+    print(subtract(num1, num2))
+
+elif operation == "*":
+    print(multiply(num1, num2))
+
+else:
+    print("Invalid Operation")
+
+
+
 
 
 
